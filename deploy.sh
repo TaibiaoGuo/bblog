d174b81b2b753bde6f0fd999425452d02d4a20d6
TaibiaoGuo <34874132+TaibiaoGuo@users.noreply.github.com>
2020-04-05 15:23:02 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
-curl -sSL https://raw.githubusercontent.com/TaibiaoGuo/bblog/master/docker-compose.yml -o docker-compose.yml \
-&& docker stack up -c docker-compose.yml bblog \
-&& rm docker-compose.yml
+mkdir bblog \
+&& cd bblog \
+&& curl -sSL https://raw.githubusercontent.com/TaibiaoGuo/bblog/master/base.env -o base.env \
+&& curl -sSL https://raw.githubusercontent.com/TaibiaoGuo/bblog/master/contracts.env -o contracts.env \
+&& curl -sSL https://raw.githubusercontent.com/TaibiaoGuo/bblog/master/docker-compose.yml -o docker-compose.yml \
+&& docker stack up -c docker-compose.yml bblog
 
